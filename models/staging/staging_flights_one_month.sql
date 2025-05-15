ace507d34b5e79ee06dba12177445b98e76aa210
Zhen Yang <zhen@ZhendeMacBook-Air.local>
2025-05-15 16:20:38 +0200

    feat: add staging model for flights_one_month

--- models/staging/staging_flights_one_month.sql
+++ models/staging/staging_flights_one_month.sql
@@ -1,8 +1,8 @@
-{{ config(materialized='view') }} -- 覆盖 dbt_project.yml 中的默认设置，将这个特定的模型物化为视图。
+{{ config(materialized='view') }}
 
 WITH flights_one_month AS (
-    SELECT * 
-    FROM {{source('flights_data', 'flights')}}
-    WHERE DATE_PART('month', flight_date) = 1 
+    SELECT *
+    FROM {{ source('flights_data', 'flights') }}
+    WHERE DATE_PART('month', flight_date) = 1
 )
-SELECT * FROM flights_one_monthgit
+SELECT * FROM flights_one_month
